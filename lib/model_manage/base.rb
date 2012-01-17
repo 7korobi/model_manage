6753488b272c1ecf9182efe29fcf5fb15c8a6c2a
ななころび <7korobi@gmail.com>
2012-01-17 22:51:40 +0900

    refactering

--- lib/model_manage/base.rb
+++ lib/model_manage/base.rb
@@ -10,57 +10,59 @@ module ModelManage
   end
   module ClassMethods
     def forms
-        const_get(:FORMS)
+      const_get(:FORMS)
     end
 
     def key?(key)
-        const_get(:KEYS).member? key.to_s
+      const_get(:KEYS).member? key.to_s
     rescue
-        const_set :KEYS, []
-        false
+      const_set :KEYS, []
+      false
     end
 
     def field(name, options = {})
-        min =   1
+      p [:field, name]
 
-        validates = {}
-        validates[:in] = options.delete(:in)  || options.delete(:within)
-        if options[:limit] && (! validates[:in])
-          validates[:in] = (min..options.delete(:limit)) 
-        end
+      min =   1
 
-        validates[:allow_nil  ] = options.delete(:allow_nil)   || false
-        validates[:allow_blank] = options.delete(:allow_blank) || false
+      validates = {}
+      validates[:in] = options.delete(:in)  || options.delete(:within)
+      if options[:limit] && (! validates[:in])
+        validates[:in] = (min..options.delete(:limit)) 
+      end
 
-        super
-        null_ok = validates[:allow_nil] || validates[:allow_blank]
-        form_attributes = {
-          owner:   self,
-          name:    name.to_s,
-          type:    options[:type] || String,
-          limit:   nil,
-          null:    null_ok,
-          primary: key?(name),
-          scale:   nil
-        }
+      validates[:allow_nil  ] = options.delete(:allow_nil)   || false
+      validates[:allow_blank] = options.delete(:allow_blank) || false
 
-        if validates[:in].present?
-          validates_length_of name, validates
-          form_attributes[:limit] = validates[:in].max
-          form_attributes.merge!(validates)
-        end
+      super
+      null_ok = validates[:allow_nil] || validates[:allow_blank]
+      form_attributes = {
+        owner:   self,
+        name:    name.to_s,
+        type:    options[:type] || String,
+        limit:   nil,
+        null:    null_ok,
+        primary: key?(name),
+        scale:   nil
+      }
 
-        forms[name] = ::OpenStruct.new(form_attributes)
+      if validates[:in].present?
+        validates_length_of name, validates
+        form_attributes[:limit] = validates[:in].max
+        form_attributes.merge!(validates)
+      end
+
+      forms[name] = ::OpenStruct.new(form_attributes)
     end
 
     def relation_form_set(name, options = {})
-        relation_attributes = {
-          owner:    self,
-          name:     name.to_s,
-          options:  options
-        }.tap{|o| o[:data] = o.dup }
-        relation = relations[name.to_s]
-        relation.form = OpenStruct.new(relation_attributes)
+      relation_attributes = {
+        owner:    self,
+        name:     name.to_s,
+        options:  options
+      }.tap{|o| o[:data] = o.dup }
+      relation = relations[name.to_s]
+      relation.form = OpenStruct.new(relation_attributes)
     end
   end
 end
