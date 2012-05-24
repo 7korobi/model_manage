644a46f360193c7960782a7929772f23bfe75a49
ななころび <7korobi@gmail.com>
2012-05-24 21:38:33 +0900

    railties

--- lib/model_manage.rb
+++ lib/model_manage.rb
@@ -3,15 +3,19 @@ require 'ostruct'
 if defined?(Mongoid::Document)
   require 'model_manage/form'              if defined?(SimpleForm) || defined?(Formtastic)
   require 'model_manage/mongoid_rails_erd' if defined?(RailsERD)
-  require 'model_manage/mongoid'   
+  require 'model_manage/mongoid'
 end
 
 if defined?(ActiveRecord::Base)
   require 'model_manage/active_record_rails_erd' if defined?(RailsERD)
-  require 'model_manage/active_record' 
+  require 'model_manage/active_record'
 end
 
 require 'model_manage/rails'
 require 'model_manage/bitfield'
 require 'model_manage/base'
 
+module ModelManage
+  class Engine < ::Rails::Engine
+  end
+end
